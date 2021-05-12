67dbc1c887b595d8746cd995d44c3763d454cf9e
David Kopec <snpukwana@gmail.com>
2021-05-12 01:36:54 -0400

    Layout for NESEmulator

---
+++ NESEmulator/__main__.py
@@ -0,0 +1,24 @@
+# NESEmulator/__main__.py
+# From Fun Computer Science Projects in Python
+# Copyright 2021 David Kopec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from argparse import ArgumentParser
+from .executioner import execute
+
+if __name__ == "__main__":
+    # Parse the file argument
+    file_parser = ArgumentParser("NESEmulator")
+    file_parser.add_argument("rom_file", help="A file containing an NES game in iNES format.")
+    arguments = file_parser.parse_args()
+    execute(arguments.basic_file)
