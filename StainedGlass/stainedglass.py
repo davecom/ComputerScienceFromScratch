db221be8510694216b3bffdc3ea930cf680ad631
David Kopec <snpukwana@gmail.com>
2021-08-10 00:19:30 -0400

    Nice version with hill climbing in all directions and multiple ways to choose starting colors for shapes

--- StainedGlass/stainedglass.py
+++ StainedGlass/stainedglass.py
@@ -13,12 +13,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Clamp an integer between 0 and 255, simulating a single byte
+from enum import Enum
 from PIL import Image, ImageDraw
 from PIL import ImageChops, ImageStat
 import random
 from math import trunc
 from timeit import default_timer as timer
+
+Direction = Enum("Direction", "LEFT RIGHT UP DOWN")
+Dimensions = tuple[tuple[int, int], tuple[int, int]]
+
+
+def change_dimensions(old: Dimensions, direction: Direction, amount: int) -> Dimensions:
+    if direction == Direction.LEFT:
+        return (old[0][0] + amount, old[0][1]), (old[1][0], old[1][1])
+    elif direction == Direction.RIGHT:
+        return (old[0][0], old[0][1]), (old[1][0] + amount, old[1][1])
+    elif direction == Direction.UP:
+        return (old[0][0], old[0][1] + amount), (old[1][0], old[1][1])
+    elif direction == Direction.DOWN:
+        return (old[0][0], old[0][1]), (old[1][0], old[1][1] + amount)
+
+
+def get_most_common_color(image: Image) -> tuple[int, int, int]:
+    colors = image.getcolors(image.width * image.height)
+    sorted_colors = sorted(colors, key=lambda item: item[0])
+    return sorted_colors[-1][1]
 
 
 class StainedGlass:
@@ -33,7 +53,7 @@ class StainedGlass:
             average_color = tuple((round(n) for n in ImageStat.Stat(self.original).mean))
             self.glass = Image.new("RGB", new_size, average_color)
             self.best_difference = self.difference(self.glass)
-            trials = 10000000
+            trials = 100000
             last_percent = 0
             start = timer()
             for test in range(trials):
@@ -46,7 +66,7 @@ class StainedGlass:
             print(f"{end-start} seconds elapsed")
             self.glass.save("/Users/dave/Downloads/glass.png")
 
-    def random_dimensions(self): #-> tuple[tuple[int, int], tuple[int, int]]:
+    def random_dimensions(self) -> Dimensions:
         x1 = random.randint(0, self.original.width)
         y1 = random.randint(0, self.original.height)
         x2 = random.randint(0, self.original.width)
@@ -54,16 +74,47 @@ class StainedGlass:
         return (x1, y1), (x2, y2)
 
     def trial(self):
-        new_image = self.glass.copy()
-        glass_draw = ImageDraw.Draw(new_image)
-        rand_color = tuple(random.choices(range(256), k=3))
-        dimensions = self.random_dimensions()
-        glass_draw.ellipse(dimensions, fill=rand_color)
-        new_difference = self.difference(new_image)
+        while True:
+            dimensions = self.random_dimensions()
+
+            (x1, y1), (x2, y2) = dimensions
+            region = self.original.crop((x1, y1, x2, y2))
+            if region.width > 0 and region.height > 0:
+                break
+        try:
+            #random color
+            #color = tuple(random.choices(range(256), k=3))
+            #average color
+            color = tuple((round(n) for n in ImageStat.Stat(region).mean))
+            #dominant color
+            #color = get_most_common_color(region)
+        except ZeroDivisionError:
+            color = (0, 0, 0)
+        original = self.glass
+
+        def experiment() -> bool:
+            new_image = original.copy()
+            glass_draw = ImageDraw.Draw(new_image)
+            glass_draw.ellipse(dimensions, fill=color)
+            new_difference = self.difference(new_image)
+            if new_difference < self.best_difference:
+                self.best_difference = new_difference
+                self.glass = new_image
+                return True
+            return False
+
         # print(dimensions, rand_color, new_difference)
-        if new_difference < self.best_difference:
-            self.best_difference = new_difference
-            self.glass = new_image
+
+        if experiment():
+            # Try expanding every direction, keep expanding in any directions that are better
+            for direction in Direction:
+                for amount in (-1, 1):
+                    while True:
+                        old_dimensions = dimensions
+                        dimensions = change_dimensions(dimensions, direction, amount)
+                        if not experiment():
+                            dimensions = old_dimensions
+                            break
 
     def difference(self, other_image: Image) -> float:
         diff = ImageChops.difference(self.original, other_image)
