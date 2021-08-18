748645b5ae85a5843e4f25b7a1ed96a5132717e1
David Kopec <snpukwana@gmail.com>
2021-08-18 00:33:46 -0400

    Atkinson Dithering Implemented

---
+++ RetroDither/__main__.py
@@ -0,0 +1,52 @@
+# RetroDither/__main__.py
+# From Fun Computer Science Projects in Python
+# Copyright 2021 David Kopec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from PIL import Image
+from argparse import ArgumentParser
+from dither import atkinson_dither
+
+MAX_WIDTH = 576
+MAX_HEIGHT = 720
+
+
+def prepare(file_name: str) -> Image:
+    with open(file_name, "rb") as fp:
+        image = Image.open(fp)
+        # Size to within the bounds of the maximum for MacPaint
+        if image.width > MAX_WIDTH or image.height > MAX_HEIGHT:
+            desired_ratio = MAX_WIDTH / MAX_HEIGHT
+            ratio = image.width / image.height
+            if ratio >= desired_ratio:
+                new_size = (MAX_WIDTH, int(image.height * (MAX_WIDTH / image.width)))
+            else:
+                new_size = (int(image.width * (MAX_HEIGHT / image.height)), MAX_HEIGHT)
+            image.thumbnail(new_size, Image.ANTIALIAS)
+        # Convert to grayscale
+        return image.convert("L")
+
+
+if __name__ == "__main__":
+    # Parse the file argument
+    argument_parser = ArgumentParser("RetroDither")
+    argument_parser.add_argument("image_file", help="The input image file.")
+    argument_parser.add_argument("output_file", help="The resulting MacPaint file.")
+    argument_parser.add_argument('-g', '--gif', default=False, action='store_true',
+                                 help='Create an output gif as well.')
+    arguments = argument_parser.parse_args()
+    original = prepare(arguments.image_file)
+    dithered_data = atkinson_dither(original)
+    if arguments.gif:
+        out_image = Image.frombytes('L', original.size, dithered_data.tobytes())
+        out_image.save(arguments.output_file + ".gif")
---
+++ RetroDither/dither.py
@@ -0,0 +1,48 @@
+# RetroDither/dither.py
+# From Fun Computer Science Projects in Python
+# Copyright 2021 David Kopec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from PIL import Image
+from array import array
+
+
+# Assumes we are working with a grayscale image (Mode "L" in Pillow)
+# Returns an array of dithered pixels (255 for black, 0 for white)
+def atkinson_dither(image: Image) -> array:
+    # Add *value* to the pixel at (*c*, *r*) in *image*
+    def add_to_pixel(c: int, r: int, value: int):
+        if c < 0 or c >= image.width or r >= image.height:
+            return
+        image.putpixel((c, r), image.getpixel((c, r)) + value)
+
+    result = array('B', [0] * (image.width * image.height))
+    for y in range(image.height):
+        for x in range(image.width):
+            old_pixel = image.getpixel((x, y))
+            # Every new pixel is either solid black or solid white
+            # since this is all that the original Macintosh supported
+            new_pixel = 255 if old_pixel > 127 else 0
+            result[y * image.width + x] = new_pixel
+            # Error is an eighth of the difference between
+            # the original pixel and how it ended up dithered
+            error = (old_pixel - new_pixel) // 8
+            # Disperse error amongst nearby upcoming pixels
+            add_to_pixel(x + 1, y, error)
+            add_to_pixel(x + 2, y, error)
+            add_to_pixel(x - 1, y + 1, error)
+            add_to_pixel(x, y + 1, error)
+            add_to_pixel(x + 1, y + 1, error)
+            add_to_pixel(x, y + 2, error)
+
+    return result
