283198ac8444f69361a1e57a074ff72d2baa6099
David Kopec <snpukwana@gmail.com>
2022-09-27 00:52:00 -0400

    Renamed dimensions to coordinates; made get_most_common_color() more efficient by using max() instead of sorting

--- StainedGlass/stainedglass.py
+++ StainedGlass/stainedglass.py
@@ -23,14 +23,13 @@ from StainedGlass.svg import SVG
 
 ColorMethod = Enum("ColorMethod", "RANDOM AVERAGE COMMON")
 ShapeType = Enum("ShapeType", "ELLIPSE TRIANGLE QUADRILATERAL LINE")
-Dimensions = list[int]
+CoordList = list[int]
 MAX_HEIGHT = 256
 
 
 def get_most_common_color(image: Image.Image) -> tuple[int, int, int]:
     colors = image.getcolors(image.width * image.height)
-    sorted_colors = sorted(colors, key=lambda item: item[0])
-    return sorted_colors[-1][1]
+    return max(colors, key=lambda item: item[0])[1]
 
 
 class StainedGlass:
@@ -66,57 +65,30 @@ class StainedGlass:
             print(f"{end-start} seconds elapsed. {len(self.shapes)} shapes created. Outputting image...")
             self.create_output(output_file, length, vector, animation_length)
 
-    def create_output(self, output_file: str, height: int, vector: bool, animation_length: int):
-        average_color = tuple((round(n) for n in ImageStat.Stat(self.original).mean))
-        original_width, original_height = self.original.size
-        ratio = height / original_height
-        output_size = (int(original_width * ratio), int(original_height * ratio))
-        output_image = Image.new("RGB", output_size, average_color)
-        output_draw = ImageDraw.Draw(output_image)
-        if vector:
-            svg = SVG(*output_size, average_color)
-        if animation_length > 0:
-            animation_frames = []
-        for coordinates, color in self.shapes:
-            dimensions = [int(x * ratio) for x in coordinates]
-            if self.shape_type == ShapeType.ELLIPSE:
-                output_draw.ellipse(dimensions, fill=color)
-                if vector:
-                    svg.draw_ellipse(*dimensions, color)
-            else:  # must be triangle or quadrilateral or line
-                if vector:
-                    if self.shape_type == ShapeType.LINE:
-                        svg.draw_line(*dimensions, color)
-                    else:
-                        svg.draw_polygon(dimensions, color)
-                output_draw.polygon(dimensions, fill=color)
-            if animation_length > 0:
-                animation_frames.append(output_image.copy())
-        output_image.save(output_file)
-        if vector:
-            svg.write(output_file + ".svg")
-        if animation_length > 0:
-            animation_frames[0].save(output_file + ".gif", save_all=True, append_images=animation_frames[1:],
-                                     optimize=False, duration=animation_length, loop=0)
+    def difference(self, other_image: Image.Image) -> float:
+        diff = ImageChops.difference(self.original, other_image)
+        stat = ImageStat.Stat(diff)
+        diff_ratio = sum(stat.mean) / (len(stat.mean) * 255)
+        return diff_ratio
 
-    def random_dimensions(self) -> Dimensions:
-        num_dimensions = 4  # ellipse or line
+    def random_coordinates(self) -> CoordList:
+        num_coordinates = 4  # ellipse or line
         if self.shape_type == ShapeType.TRIANGLE:
-            num_dimensions = 6
+            num_coordinates = 6
         elif self.shape_type == ShapeType.QUADRILATERAL:
-            num_dimensions = 8
-        dimensions = []
-        for d in range(num_dimensions):
+            num_coordinates = 8
+        coordinates = []
+        for d in range(num_coordinates):
             if d % 2 == 0:  # x coordinates
-                dimensions.append(random.randint(0, self.original.width))
+                coordinates.append(random.randint(0, self.original.width))
             else:  # y coordinates
-                dimensions.append(random.randint(0, self.original.height))
-        return dimensions
+                coordinates.append(random.randint(0, self.original.height))
+        return coordinates
 
     @staticmethod
-    def bounding_box(dimensions: Dimensions) -> tuple[int, int, int, int]:
-        xcoords = dimensions[::2]
-        ycoords = dimensions[1::2]
+    def bounding_box(coordinates: CoordList) -> tuple[int, int, int, int]:
+        xcoords = coordinates[::2]
+        ycoords = coordinates[1::2]
         x1 = min(xcoords)
         y1 = min(ycoords)
         x2 = max(xcoords)
@@ -125,8 +97,8 @@ class StainedGlass:
 
     def trial(self):
         while True:
-            dimensions = self.random_dimensions()
-            region = self.original.crop(self.bounding_box(dimensions))
+            coordinates = self.random_coordinates()
+            region = self.original.crop(self.bounding_box(coordinates))
             if region.width > 0 and region.height > 0:
                 break
 
@@ -142,9 +114,9 @@ class StainedGlass:
             new_image = original.copy()
             glass_draw = ImageDraw.Draw(new_image)
             if self.shape_type == ShapeType.ELLIPSE:
-                glass_draw.ellipse(dimensions, fill=color)
+                glass_draw.ellipse(coordinates, fill=color)
             else:  # must be triangle or quadrilateral or line
-                glass_draw.polygon(dimensions, fill=color)
+                glass_draw.polygon(coordinates, fill=color)
             new_difference = self.difference(new_image)
             if new_difference < self.best_difference:
                 self.best_difference = new_difference
@@ -152,22 +124,49 @@ class StainedGlass:
                 return True
             return False
 
-        # print(dimensions, rand_color, new_difference)
+        # print(coordinates, rand_color, new_difference)
 
         if experiment():
             # Try expanding every direction, keep expanding in any directions that are better
-            for index in range(len(dimensions)):
+            for index in range(len(coordinates)):
                 for amount in (-1, 1):
                     while True:
-                        old_dimensions = dimensions.copy()
-                        dimensions[index] = dimensions[index] + amount
+                        old_coordinates = coordinates.copy()
+                        coordinates[index] = coordinates[index] + amount
                         if not experiment():
-                            dimensions = old_dimensions
+                            coordinates = old_coordinates
                             break
-            self.shapes.append((dimensions, color))
+            self.shapes.append((coordinates, color))
 
-    def difference(self, other_image: Image.Image) -> float:
-        diff = ImageChops.difference(self.original, other_image)
-        stat = ImageStat.Stat(diff)
-        diff_ratio = sum(stat.mean) / (len(stat.mean) * 255)
-        return diff_ratio
+    def create_output(self, output_file: str, height: int, vector: bool, animation_length: int):
+        average_color = tuple((round(n) for n in ImageStat.Stat(self.original).mean))
+        original_width, original_height = self.original.size
+        ratio = height / original_height
+        output_size = (int(original_width * ratio), int(original_height * ratio))
+        output_image = Image.new("RGB", output_size, average_color)
+        output_draw = ImageDraw.Draw(output_image)
+        if vector:
+            svg = SVG(*output_size, average_color)
+        if animation_length > 0:
+            animation_frames = []
+        for coordinate_list, color in self.shapes:
+            coordinates = [int(x * ratio) for x in coordinate_list]
+            if self.shape_type == ShapeType.ELLIPSE:
+                output_draw.ellipse(coordinates, fill=color)
+                if vector:
+                    svg.draw_ellipse(*coordinates, color)
+            else:  # must be triangle or quadrilateral or line
+                if vector:
+                    if self.shape_type == ShapeType.LINE:
+                        svg.draw_line(*coordinates, color)
+                    else:
+                        svg.draw_polygon(coordinates, color)
+                output_draw.polygon(coordinates, fill=color)
+            if animation_length > 0:
+                animation_frames.append(output_image.copy())
+        output_image.save(output_file)
+        if vector:
+            svg.write(output_file + ".svg")
+        if animation_length > 0:
+            animation_frames[0].save(output_file + ".gif", save_all=True, append_images=animation_frames[1:],
+                                     optimize=False, duration=animation_length, loop=0)
