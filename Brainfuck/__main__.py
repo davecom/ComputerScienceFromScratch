0adbffe2591e9c3450780ef1e15a5cf62eb8d2d5
David Kopec <snpukwana@gmail.com>
2025-01-11 23:50:16 -0500

    Fix typo in Brainfuck __main__.py

--- Brainfuck/__main__.py
+++ Brainfuck/__main__.py
@@ -18,7 +18,7 @@ from Brainfuck.brainfuck import Brainfuck
 
 if __name__ == "__main__":
     # Parse the file argument
-    file_parser = ArgumentParser("NanoBASIC")
+    file_parser = ArgumentParser("Brainfuck")
     file_parser.add_argument("brainfuck_file",
                              help="A file containing Brainfuck source code.")
     arguments = file_parser.parse_args()
